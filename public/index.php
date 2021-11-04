7cebad1cd5605da961874747cef1d6a1f264d890
theBenHur <benhurt742@gmail.com>
2021-11-04 14:50:50 -0600

    Uso de Slim4

---
+++ public/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Hello, World!');
+    return $response;
+})->setName('root');
+
+// Run app
+$app->run();
